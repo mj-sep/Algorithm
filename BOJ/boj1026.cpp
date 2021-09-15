3d6a851f80f8d0691f583d4629e979d824b16db6
minjoo <71536608+mj-sep@users.noreply.github.com>
2021-09-15 23:13:18 +0900

    BOJ 9012 : 괄호
    stack

--- BOJ/boj1026.cpp
+++ BOJ/boj1026.cpp
@@ -1,55 +1,53 @@
 #include<iostream>
+#include<stack>
+#include<string>
 #include<vector>
 using namespace std;
 
-vector<int> v;
-vector<int> v2;
-int n, n2, n3;
+vector<string> v;
+string openclose[51];
+string c;
 
-// quick sort (vector -> call by value X, call by reference O)
-void quick_sort(vector<int> &arr,int start, int end){
-    // 원소가 1개면 리턴
-    if (start >= end) return;
-    int pivot = start;
-    int left = start + 1;
-    int right = end;
-    
-    while (left <= right) {
-        // 왼쪽부터 pivot보다 큰 수 찾기
-        while (left <= end && arr[left] <= arr[pivot]) left++;
-        // 오른쪽부터 pivot보다 작은 수 찾기
-        while (right > start && arr[right] >= arr[pivot]) right--;
-        // left와 right이 서로 엇갈리면 pivot과 작은 수 교환
-        if (left > right) swap(arr[pivot], arr[right]);
-        // 그렇지 않으면 left와 right 교환
-        else swap(arr[left], arr[right]);
-    }
-    quick_sort(arr, start, right - 1);
-    quick_sort(arr, right + 1, end);
+string stack_confirm(string arr[], string num) {
+	stack<string> s;
+	for (int i = 0; i < num.size(); i++) {
+		// 여는 괄호일 때
+		if (arr[i] == "(") s.push(arr[i]);
+		// 닫는 괄호일 때
+		else if (arr[i] == ")") {
+			if (!s.empty()) s.pop();
+			else {
+				return "NO";
+			}
+		}
+	}
+	
+	if (s.empty()) {
+		return "YES";
+	}
+	else return "NO";
+	
 }
 
 int main() {
-    int sum = 0;
-    cin >> n;
+	
+	int t;
+	cin >> t;
 
-    for (int i = 0; i < n; i++) {
-        cin >> n2;
-        v.push_back(n2);
-    }
+	for (int i = 0; i < t; i++) {
+		cin >> c;
+		// 문자 하나씩 끊어서 배열에 입력
+		for (int j = 0; j < c.size(); j++) {
+			openclose[j] = c.substr(j, 1);
+		}
+		// 입력이 끝나고 한번에 출력해야 하므로 결과를 벡터에 저장해놓음.
+		string str = stack_confirm(openclose, c);
+		v.push_back(str);
 
-    for (int j = 0; j < n; j++) {
-        cin >> n3;
-        v2.push_back(n3);
-    }
-
-    quick_sort(v, 0, n - 1);
-    quick_sort(v2, 0, n - 1);
-    //sort(v2.begin(), v2.end(), greater<int>()); // 역순
-
-   
-    for (int i = 0; i < v.size(); i++) {
-        sum += v[i] * v2[n - 1 - i];
-    }
-    cout << sum;
-
+	}
+	
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << "\n";
+	}
+	return 0;
 }
