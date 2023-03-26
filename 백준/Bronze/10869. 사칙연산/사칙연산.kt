a4b0a3b416a27157f6faa1e3abe6dc4cde669f4a
Minjoo Seo <71536608+mj-sep@users.noreply.github.com>
2023-03-26 22:32:24 +0900

    [Bronze V] Title: 사칙연산, Time: 116 ms, Memory: 13256 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main() {
+
+    var input = Scanner(System.`in`)
+    var inputA = input.nextInt()
+    var inputB = input.nextInt()
+
+    println(inputA + inputB)
+    println(inputA - inputB)
+    println(inputA * inputB)
+    println(inputA / inputB)
+    println(inputA % inputB)
+
+}
